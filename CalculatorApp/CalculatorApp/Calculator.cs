2ff8e911023664220b43151fd6ce6c2993465b9d
HarunChowdhury <trishamomo1@gmail.com>
2014-09-27 14:14:38 +0600

    uploaded Apps

---
+++ CalculatorApp/CalculatorApp/Calculator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CalculatorApp
+{
+    class Calculator
+    {
+        public double Add(double firstNo, double secondNo)
+        {
+            return firstNo + secondNo;
+        }
+
+        public double Subtract(double firstNo, double secondNo)
+        {
+            return firstNo - secondNo;
+        }
+        public double Multiply(double firstNo, double secondNo)
+        {
+            return firstNo * secondNo;
+        }
+        public double Divide(double firstNo, double secondNo)
+        {
+            if (secondNo == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            return firstNo / secondNo;
+        }
+
+    }
+}
---
+++ CalculatorApp/CalculatorApp/CalculatorUI.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CalculatorApp
+{
+    public partial class CalculatorUI : Form
+    {
+        public CalculatorUI()
+        {
+            InitializeComponent();
+        }
+
+        Calculator aCalculator = new Calculator();
+
+        private double firstNo;
+        private double secondNo;
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            InitializeFirstNoAndSecondNo();
+            MessageBox.Show(aCalculator.Add(firstNo, secondNo).ToString());
+        }
+
+        private void InitializeFirstNoAndSecondNo()
+        {
+            firstNo = Convert.ToDouble(firstNumberTextBox.Text);
+            secondNo = Convert.ToDouble(secondNumberTextBox.Text);
+        }
+
+        private void subtractButton_Click(object sender, EventArgs e)
+        {
+            InitializeFirstNoAndSecondNo();
+            MessageBox.Show(aCalculator.Subtract(firstNo, secondNo).ToString());
+        }
+
+        private void divideButton_Click(object sender, EventArgs e)
+        {
+            InitializeFirstNoAndSecondNo();
+            try
+            {
+                double result = aCalculator.Divide(firstNo, secondNo);
+                MessageBox.Show(result.ToString());
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Please enter non-zero as second no");
+
+            }
+            
+            
+        }
+    }
+}
